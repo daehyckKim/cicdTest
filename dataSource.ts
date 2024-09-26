c672e647c43f27935a94bed31e016e2aa5bc0b08
daehyck <kilcs12@addd.co.kr>
2024-09-26 17:22:07 +0900

    cicd 1차 테스트

---
+++ dataSource.ts
@@ -0,0 +1,25 @@
+import { User } from './src/user/entitly/user.entity';
+import { DataSource } from 'typeorm';
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const dataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.dbHost,
+  port: 3306,
+  username: process.env.dbUser,
+  password: process.env.dbPassword,
+  database: process.env.database,
+  entities: [User],
+  migrations: [__dirname + '/src/migrations/*.ts'],
+  charset: 'utf8mb4',
+  synchronize: false,
+  // logging: true,
+  logging: false,
+  extra: {
+    collation: 'utf8mb4_unicode_ci', // Add the collation property here
+  },
+});
+
+export default dataSource;
--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,20 +1,12 @@
-import { DataSource } from 'typeorm';
+import dataSource from 'dataSource';
+
+const dotenv = require('dotenv');
+dotenv.config();
 
 export const databaseProviders = [
   {
     provide: 'DATA_SOURCE',
     useFactory: async () => {
-      const dataSource = new DataSource({
-        type: 'mysql',
-        host: process.env.dbHost,
-        port: 3306,
-        username: process.env.dbUser,
-        password: process.env.dbPassword,
-        database: process.env.database,
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: false,
-      });
-
       return dataSource.initialize();
     },
   },
